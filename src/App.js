9bda55d4f9f96b8c2b77103be1360e8bbf4ba4f6
Akin Evans <akinevans@gmail.com>
2024-04-12 13:56:13 -0400

    Build out basic functionality and simple UI

--- src/App.js
+++ src/App.js
@@ -4,10 +4,15 @@ import Medication from "./components/medication/Medication";
 function App() {
   return (
     <div className='App'>
-      <h1 className='app-title'>Dose Dash</h1>
+      <h1 className='app-title'>Dose Dash 💊</h1>
+      <h2 className='app-subtitle'>Have you taken your meds today? </h2>
       <div className='list-of-meds-wrapper'>
         {/* medication components here */}
-        <Medication />
+        <Medication name='Citalopram' dosage='20' />
+        <Medication name='Trazodone' dosage='50' />
+        <Medication name='Alprazolam' dosage='0.25' />
+        <Medication name='Tylenol' dosage='400' />
+        <Medication name='Biotin' dosage='5' />
       </div>
     </div>
   );
--- src/components/medication/Medication.js
+++ src/components/medication/Medication.js
@@ -1,6 +1,37 @@
 import React from "react";
+import { useState } from "react";
 import "./Medication.css";
 
-export default function Medication() {
-  return <div>Medication</div>;
+//TODO implement saving med status via local storage.
+
+export default function Medication(props) {
+  const [medsTaken, setMedsTaken] = useState(false);
+
+  const messages = {
+    take: "Take",
+    reset: "Reset",
+    status: <p className='status-message-banner'>Taken</p>,
+  };
+
+  return (
+    <div className='medication-component-wrapper'>
+      <div className='med-wrapper-left'>
+        <h1 className='med-name'>{props.name}</h1>
+        <h2 className='med-dosage'>{`${props.dosage}mg`}</h2>
+      </div>
+      <div className='med-wrapper-right'>
+        <button
+          className='toggle-switch'
+          onClick={() => {
+            setMedsTaken(!medsTaken);
+          }}
+        >
+          {`${!medsTaken ? messages.take : messages.reset}`}
+        </button>
+
+        {/* Meds taken banner */}
+        {medsTaken ? messages.status : null}
+      </div>
+    </div>
+  );
 }
